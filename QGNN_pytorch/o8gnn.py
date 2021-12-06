358916510e42f2d71fa81af49c82bc0a4d961dfa
Daggerfall-is-the-Best-TES-Game <Davidcfried@gmail.com>
2021-12-06 18:52:35 -0500

    adjust make_octonion_mult hamilton construction

--- QGNN_pytorch/o8gnn.py
+++ QGNN_pytorch/o8gnn.py
@@ -7,31 +7,38 @@ from torch.nn.modules.module import Module
 
 """@Dai Quoc Nguyen"""
 '''Make a Hamilton matrix for Octonion linear transformations'''
+
+
 def make_octonion_mul(kernel):
     """" The constructed 'hamilton' W is a modified version of the Octonion representation,
-        thus doing tf.matmul(Input,W) is equivalent to W * Inputs. """
-    dim = kernel.size(1)//8
-    a0, a1, a2, a3, a4, a5, a6, a7 = torch.split(kernel, [dim, dim, dim, dim, dim, dim, dim, dim], dim=1)
-    a0_ = torch.cat([a0, -a1, -a2, -a3, -a4, -a5, -a6, -a7], dim=0)
-    a1_ = torch.cat([a1, a0, a3, -a2, a5, -a4, -a7, a6], dim=0)
-    a2_ = torch.cat([a2, -a3, a0, a1, a6, a7, -a4, -a5], dim=0)
-    a3_ = torch.cat([a3, a2, -a1, a0, a7, -a6, a5, -a4], dim=0)
-    a4_ = torch.cat([a4, -a5, -a6, -a7, a0, a1, a2, a3], dim=0)
-    a5_ = torch.cat([a5, a4, -a7, a6, -a1, a0, -a3, a2], dim=0)
-    a6_ = torch.cat([a6, a7, a4, -a5, -a2, a3, a0, -a1], dim=0)
-    a7_ = torch.cat([a7, -a6, a5, a4, -a3, -a2, a1, a0], dim=0)
-    hamilton = torch.cat([a0_, a1_, a2_, a3_, a4_, a5_, a6_, a7_], dim=1)
+        thus doing tf.matmul(Input,W) is equivalent to W * Inputs.
+        kernel = batch X hidden_size"""
+    dim = kernel.size(1) // 8
+
+    a0, a1, a2, a3, a4, a5, a6, a7 = torch.split(kernel, dim, dim=1)
+    hamiliton = [[a0, a1, a2, a3, a4, a5, a6, a7],
+                 [a1, -a0, a3, -a2, a5, -a4, -a7, a6],
+                 [a2, -a3, -a0, a1, a6, a7, -a4, -a5],
+                 [a3, a2, -a1, -a0, a7, -a6, a5, -a4],
+                 [a4, -a5, -a6, -a7, -a0, a1, a2, a3],
+                 [a5, a4, -a7, a6, -a1, -a0, -a3, a2],
+                 [a6, a7, a4, -a5, -a2, a3, -a0, -a1],
+                 [a7, -a6, a5, a4, -a3, -a2, a1, -a0]]
+    hamilton = torch.cat([torch.cat(row) for row in hamiliton], dim=1)
     assert kernel.size(1) == hamilton.size(1)
     return hamilton
 
+
 '''Octonion graph neural networks! OGNN layer for other downstream tasks!'''
+
+
 class OGNNLayer_v2(Module):
     def __init__(self, in_features, out_features, act=torch.tanh):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
         self.act = act
-        self.weight = Parameter(torch.FloatTensor(self.in_features//8, self.out_features))
+        self.weight = Parameter(torch.FloatTensor(self.in_features // 8, self.out_features))
         self.reset_parameters()
         self.bn = torch.nn.BatchNorm1d(out_features)
 
@@ -46,7 +53,10 @@ class OGNNLayer_v2(Module):
         output = self.bn(output)  # using act torch.tanh with BatchNorm can produce competitive results
         return self.act(output)
 
+
 '''Octonion graph neural networks! OGNN layer for node and graph classification tasks!'''
+
+
 class OGNNLayer(Module):
     def __init__(self, in_features, out_features, dropout, octonion_ff=True, act=F.relu):
         super().__init__()
@@ -58,7 +68,7 @@ class OGNNLayer(Module):
         self.bn = torch.nn.BatchNorm1d(out_features)
         #
         if self.octonion_ff:
-            self.weight = Parameter(torch.FloatTensor(self.in_features//8, self.out_features))
+            self.weight = Parameter(torch.FloatTensor(self.in_features // 8, self.out_features))
         else:
             self.weight = Parameter(torch.FloatTensor(self.in_features, self.out_features))
         self.reset_parameters()
@@ -69,7 +79,7 @@ class OGNNLayer(Module):
 
     def forward(self, input, adj):
 
-        x = self.dropout(input) # Current Pytorch 1.5.0 doesn't support Dropout for sparse matrix
+        x = self.dropout(input)  # Current Pytorch 1.5.0 doesn't support Dropout for sparse matrix
 
         if self.octonion_ff:
             hamilton = make_octonion_mul(self.weight)
@@ -83,7 +93,10 @@ class OGNNLayer(Module):
 
         return self.act(output)
 
+
 '''Dual quaternion multiplication'''
+
+
 def dual_quaternion_mul(A, B, input):
     '''(A, B) * (C, D) = (A * C, A * D + B * C)'''
     dim = input.size(1) // 2
@@ -96,7 +109,10 @@ def dual_quaternion_mul(A, B, input):
     AD_plus_BC = AD + BC
     return torch.cat([AC, AD_plus_BC], dim=1)
 
+
 ''' Dual Quaternion Graph Neural Networks! https://arxiv.org/abs/2104.07396 '''
+
+
 class DQGNN_layer(Module):
     def __init__(self, in_features, out_features, act=torch.tanh):
         super().__init__()
@@ -104,7 +120,7 @@ class DQGNN_layer(Module):
         self.out_features = out_features
         self.act = act
 
-        self.A = Parameter(torch.FloatTensor(self.in_features // 8, self.out_features // 2)) # (A, B) = A + eB, e^2 = 0
+        self.A = Parameter(torch.FloatTensor(self.in_features // 8, self.out_features // 2))  # (A, B) = A + eB, e^2 = 0
         self.B = Parameter(torch.FloatTensor(self.in_features // 8, self.out_features // 2))
 
         self.reset_parameters()
@@ -121,7 +137,10 @@ class DQGNN_layer(Module):
         output = self.bn(output)
         return self.act(output)
 
+
 ''' Simplifying Quaternion Graph Neural Networks! following SGC https://arxiv.org/abs/1902.07153'''
+
+
 class SQGNN_layer(Module):
     def __init__(self, in_features, out_features, step_k=1):
         super(SQGNN_layer, self).__init__()
@@ -130,7 +149,7 @@ class SQGNN_layer(Module):
         self.step_k = step_k
         self.weight = Parameter(torch.FloatTensor(self.in_features // 4, self.out_features))
         self.reset_parameters()
-        #self.bn = torch.nn.BatchNorm1d(out_features)
+        # self.bn = torch.nn.BatchNorm1d(out_features)
 
     def reset_parameters(self):
         stdv = math.sqrt(6.0 / (self.weight.size(0) + self.weight.size(1)))
@@ -140,8 +159,8 @@ class SQGNN_layer(Module):
         hamilton = make_octonion_mul(self.weight)
         new_input = torch.spmm(adj, input)
         if self.step_k > 1:
-            for _ in range(self.step_k-1):
+            for _ in range(self.step_k - 1):
                 new_input = torch.spmm(adj, new_input)
         output = torch.mm(new_input, hamilton)  # Hamilton product, quaternion multiplication!
-        #output = self.bn(output)
+        # output = self.bn(output)
         return output
